db7d450f89e24c87dd9ba435449830586dd0308c
komaitaira <komaitaira@gmail.com>
2021-08-08 23:18:42 +0900

    Finish Tuple, objectの型推論

--- section3/example.ts
+++ section3/example.ts
@@ -29,3 +29,34 @@ const flag = false;
 const a1 = [0 as 0, 1 as 1] // const a1: (0 | 1)[]
 a1.push(1);
 a1.push(2); // Error
+
+/**
+ * 3-2-2 Tupleの型推論
+ * Tupleとして確約されているindex値参照を行うと、代入された値は型を推論する。index外の値を参照しようとすると、コンパイルエラーを得ることができる。
+ */
+const t1 = [false] as [boolean];
+const t2 = [false, 1] as [boolean, number];
+const t3 = [false, 1, "2"] as [boolean, number, string];
+
+const v3_0 = t3[0];
+const v3_1 = t3[1];
+const v3_2 = t3[2];
+const v3_3 = t3[3]; // Error
+
+/**
+ * 3-3 objectの型推論
+ * objectの変数宣言時に初期値を与えることで、型推論が適用される。保持するプロパティがLiteralTypesとして推論されるためには、アサーションを使用する
+ */
+// const obj = {
+//   foo: false,
+//   bar: 1,
+//   baz: "2"
+// }
+// obj["foo"] = true;
+
+const obj = {
+  foo: false as false,
+  bar: 1 as 1,
+  baz: "2" as "2"
+}
+obj["foo"] = true;
