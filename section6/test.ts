89a502edfe2f18c348930c6e2399041aeb2b921c
komaitaira <komaitaira@gmail.com>
2021-08-10 18:10:30 +0900

    Finish 従来の組み込みUtilityTypes

--- section6/test.ts
+++ section6/test.ts
@@ -32,3 +32,32 @@ console.log(box1)
 type IsString<T> = T extends string ? true : false
 type X = IsString<"test">
 type Y = IsString<0>
+
+/**
+ * 6-3-1 従来の組み込み Utility Types
+ * 型のライブラリ
+ */
+
+// Readonly型　Object型のプロパティを、全てreadonlyに変換し、新しい型を生成する型
+interface User {
+  name: string
+  age: number | null
+  gender: "male" | "female" | "other"
+  birthplace?: string
+}
+type ReadonlyWrapUser = Readonly<User>
+
+// Partial型 Object型のプロパティを全てoptionalに変換し、新しい型を生成する型
+type PartialWrapUser = Partial<User>
+
+// Required型 Object型のプロパティから、全てoptionalを取り除き、新しい型を生成する型
+type RequiredWrapUser = Required<User>
+
+// Record型 第一Genericsに指定したプロパティ名称で、新しいObjectを生成する型
+type UserRecord = Record<"user", User>
+
+// Pick型 第二Genericsに指定した名称のプロパティ型を、第一Genericsに指定した型から抽出し、新しいObjectを生成する型
+type UserGender = Pick<User, "gender">
+
+// Omit型 第二Genericsに指定した名称のプロパティ型を、第一Genericsから取り除き、新しいObject型を生成する型
+type WithoutBirthplace = Omit<User, "birthplace">
